fa25dfcf8c7fe093574869390c575e9d593689b1
JamesPagani <jaimeagalvezv@gmail.com>
2021-01-29 03:14:38 +0000

    Code only accepts specific status codes again.

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,11 +11,13 @@ def status_report(total_size, status_codes):
     """Print a report of the last ten HTTP requests recieved"""
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
-        print("{}: {}".format(code, status_codes[code]))
+        if status_codes[code] > 0:
+        	print("{}: {}".format(code, status_codes[code]))
 
 # Variables
 total_size = 0
-status_codes = {}
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                "404": 0, "405": 0, "500": 0}
 count = 0
 
 try:
@@ -23,17 +25,16 @@ try:
         # Take the last two items, which are the status code and file size...
         metrics = line.split()[-2:]
 
-        # Create a new key-pair value, if it doesn't exist yet.
-        if metrics[0] not in status_codes:
-            status_codes[metrics[0]] = 1
-        else:
+        if metrics[0] in status_codes:
             status_codes[metrics[0]] += 1
+
         total_size += int(metrics[1])
         count += 1
 
         # Each ten lines, report the total file size and amount of status codes
         if count % 10 == 0:
             status_report(total_size, status_codes)
+
 except KeyboardInterrupt:
     status_report(total_size, status_codes)
     raise
