05bc903a0665a9ae3d53cfb6621f2cebdefa09fa
JamesPagani <jaimeagalvezv@gmail.com>
2021-05-06 03:15:02 +0000

    Keyword counting function (and backup functions)

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""Count it.
+
+Write a recursive function that queries the Reddit API, parses the title of
+all hot articles, and prints a sorted count of given keywords.
+"""
+
+import requests
+
+
+def sortRules(item):
+    """Sort rules for the sorted method."""
+    keyword = item[0]
+    count = item[1]
+    return (count, keyword)
+
+
+def setKeywords(word_list):
+    """Convert the word list into a list of tuples.
+
+    Tuple format: (keyword, count, increment)
+    """
+    keywords = []
+    if type(word_list[0]) is not list:
+        word_list.sort()
+        lastWord = None
+        for word in word_list:
+            if word == lastWord:
+                keywords[len(keywords) - 1][2] += 1
+            else:
+                keywords.append([word, 0, 1])
+            lastWord = word
+    else:
+        keywords = word_list
+    return keywords
+
+
+def count_words(subreddit, word_list):
+    """count_words.
+
+    Count the amount of times keywords appear in the hot articles
+    of a subreddit and prints the results.
+
+
+    args:
+    - subreddit (string): Name of the subreddit to check.
+    - word_list (list): A list of strings filled with keywords.
+    """
+    req_info = subreddit.split()
+    keywords = setKeywords(word_list)
+
+    # Making the Request
+    url = "https://www.reddit.com/r/{}/hot.json".format(req_info[0])
+    headers = {
+        "user-agent": "Linux:1fmuwOiskMHGTQ:v0.0.1 (by /u/JamesWulfgaro)"
+        }
+    params = {}
+    if len(req_info) > 1:
+        params["before"] = req_info[1]
+    sub_info = requests.get(
+        url,
+        allow_redirects=False,
+        headers=headers,
+        params=params
+        )
+
+    # If the subreddit does not exist, don't even continue
+    if sub_info.status_code != 200:
+        return
+
+    # Keyword counting
+    next_page = sub_info.json().get('data').get('after', None)
+    articles = sub_info.json().get('data').get('children')
+    for article in articles:
+        for word in article.get('data').get('title').split():
+            for keyword in keywords:
+                if word.lower() == keyword[0].lower():
+                    keyword[1] += keyword[2]
+
+    # Print the results if there's no more pages
+    if next_page is None:
+        sorted_keywords = sorted(keywords, key=sortRules)
+        for keyword in sorted_keywords:
+            if keyword[1] > 0:
+                print("{}: {}".format(keyword[0], keyword[1]))
+        return
+
+    # Preparing the next request
+    subreddit = "{} {}".format(subreddit, next_page)
+    count_words(subreddit, keywords)
