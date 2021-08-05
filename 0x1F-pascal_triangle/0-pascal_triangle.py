51f2e4dc6e4500a9fc6695d4e90fae7a5253d1ab
JamesPagani <jaimeagalvezv@gmail.com>
2021-08-05 00:25:18 +0000

    Answer files for algorithm project 0x1F.

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Pascal Triangle.
+
+Answer file for Algorithm Project 0x1F.
+"""
+
+
+def pascal_triangle(n):
+    """pascal_triangle.
+    
+    Return a pascal tirangle of depth n.
+
+    Args:
+        n: Depth of the Pascal triangle.
+
+    Return: List of lists of integers representing the Pascal triangle.
+    """
+    pascal = []
+
+    for i in range(0, n):
+        pascal.append([])
+        for j in range(0, i + 1):
+            if j == 0 or j == i:
+                pascal[i].append(1)
+            else:
+                pascal[i].append(pascal[i - 1][j] + pascal[i - 1][j - 1])
+    return pascal
---
+++ 0x1F-pascal_triangle/README.md
@@ -0,0 +1,7 @@
+# 0x1F. Pascal's Triangle
+
+## The Challenge
+Create a Python function that returns a list of lists representing the Pascal's triangle of n, where n is the depth of the triangle.
+
+***
+***Jaime Andrés Gálvez Villamarin***
